b7d54fee0f301f909e4b0eb4aa13ab9e8f4632d6
Letuananh1207 <tuananh12072002@gmail.com>
2025-10-30 10:13:00 +0700

    Fix gg login

--- src/app.js
+++ src/app.js
@@ -52,7 +52,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://tieuquyhantuluc.onrender.com/api/auth/google/callback"
+          : "http://localhost:3000/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
